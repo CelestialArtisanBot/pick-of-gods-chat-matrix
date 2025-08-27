e576ba1923a0c9bd386ed66f8efa5cf695588288
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-27 16:56:49 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,4 @@
+
 const chatMessages = document.getElementById("chat-messages");
 const userInput = document.getElementById("user-input");
 const sendButton = document.getElementById("send-button");
